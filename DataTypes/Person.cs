5f0ac3ee9c77976ddb89eee07229db57616663cb
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-16 18:41:31 +0200

    Add project files.

---
+++ DataTypes/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Person
+    {
+        public string Name;
+        public string LastName { get; set; }
+        public int BirthDate { get; set; }
+        public Person()
+        {
+            BirthDate = 1950;
+        }
+        public int GetAge()
+        {
+            return DateTime.Today.Year - BirthDate;
+        }
+
+
+    }
+}
---
+++ DataTypes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //bool isTrue = true;
+            //isTrue = false;
+            //Console.WriteLine(isTrue);
+            //int a = 10;
+            //a++;
+            //decimal b = a;
+            //int c = 2;
+            //string d = "Roman";
+            //int e = a / c;
+
+            Person roman = new Person();
+            roman.Name = "Roman";
+            roman.LastName = "Nowak";
+            roman.BirthDate = 2000;
+
+            Person anna = roman;
+            anna.Name = "Anna";
+            anna.LastName = "Marek";
+
+            int c = 10;
+            int b = c;
+            b = 5;
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+
+            //Console.WriteLine(roman.Name);
+            //Console.WriteLine(roman.LastName);
+            //Console.WriteLine(roman.GetAge());
+
+            //Console.WriteLine(anna.Name);
+            //Console.WriteLine(anna.LastName);
+            //Console.WriteLine(anna.GetAge());
+            Console.ReadLine();
+        }
+    }
+}
